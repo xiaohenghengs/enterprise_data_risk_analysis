dcc24b69405f0f2ac9e1aeb12709cd69207fe7b5
softwareheng <649047606@qq.com>
2022-01-20 14:53:33 +0800

    新数据初始化

--- edra/initialization/starter/initialization_v1.py
+++ edra/initialization/starter/initialization_v1.py
@@ -55,7 +55,12 @@ def initCustomsDeclarationTarget():
     init_quantile_fobdj = DynamicRangeQuantile(fob, 10, 4, None, 2)
     with DataBaseOperate() as db:
         # query data group by enterprise
-        customs_enterprise_codes = db.query_all('SELECT HGQYDM FROM customs_declaration_v1 GROUP BY HGQYDM')
+        customs_enterprise_codes = db.query_all(
+            """
+            SELECT HGQYDM, DATE_FORMAT(CKRQ_1, '%Y') AS YEAR
+            FROM customs_declaration_v1
+            GROUP BY HGQYDM, DATE_FORMAT(CKRQ_1, '%Y')
+            """)
         logger.info('》》》》》》》》》查询获取全部 %d 家企业' % len(customs_enterprise_codes))
         # query total trade price by enterprise
         total_trade_price = db.query_all(
@@ -67,7 +72,7 @@ def initCustomsDeclarationTarget():
             """)
         total_trade_price = numpy.array(total_trade_price)
         logger.info('》》》》》》》》》初始化全部企业年出口额成功！')
-        customs_group = listOfGroups(customs_enterprise_codes, 75)
+        customs_group = listOfGroups(customs_enterprise_codes, 250)
         threads = []
         for customs in customs_group:
             threads.append(Thread(target=threadInitTarget, args=(customs,)))
@@ -77,15 +82,13 @@ def initCustomsDeclarationTarget():
             thread.join()
 
 
-def threadInitTarget(customs_enterprise_codes):
+def threadInitTarget(codes_year):
     thread_name = threading.current_thread().name
     logger.info('》》》》》》%s start!' % thread_name)
-    global init_quantile_cksl, init_quantile_mylaj_sb, init_quantile_ckjhje, init_quantile_jsje, init_quantile_zsse, \
-        init_quantile_ytse, init_quantile_mylaj, init_quantile_sl1, init_quantile_sl2, init_quantile_sbsl, \
-        init_quantile_mz_2, init_quantile_jz, init_quantile_fobdj, total_trade_price
-    for index, code in enumerate(customs_enterprise_codes):
-        code = code[0]
-        logger.info('》》》thread：%s ，第 %d 家企业，海关企业代码：%s' % (thread_name, index, code))
+    for index, code_year in enumerate(codes_year):
+        code = code_year[0]
+        year = code_year[1]
+        logger.info('》》》thread：%s ，第 %d 家企业，海关企业代码：%s' % (thread_name, index, code_year))
         with DataBaseOperate() as db:
             # query data by customs code
             details = db.query_all(
@@ -97,66 +100,78 @@ def threadInitTarget(customs_enterprise_codes):
                             TIMESTAMPDIFF(MONTH, DATE_FORMAT(CKRQ_1, '%Y-%m-%d'), CONCAT(LEFT(SBNY, 4), '-', RIGHT(SBNY, 2), '-01')) AS DIFF,
                             round(MYLAJ / SL1, 4) as FOBDJ, DATE_FORMAT(CKRQ_1, '%Y') AS CKN
                     FROM customs_declaration_v1
-                    WHERE HGQYDM = '""" + code + "'")
-            logger.info('》》》thread：%s ，查询到该企业含有 %d 条报关单明细，开始明细分析' % (thread_name, len(details)))
-            details_frame = numpy.array(details)
-        customs = CustomsDeclarationTarget()
-        for detail in details_frame:
-            customs.hgqydm = detail[0]
-            zmy_frame = total_trade_price[total_trade_price[:, 0] == detail[0]][:, 1:3]
-            customs.sbny = detail[1]
-            customs.pc = detail[2]
-            customs.tsjgdm = detail[3]
-            customs.glh = detail[4]
-            customs.ckfph = detail[5]
-            customs.bgdh = detail[6]
-            customs.spdm = detail[7]
-            customs.spmc = detail[8]
-            customs.jldw = detail[9]
-            customs.cksl_decile = init_quantile_cksl.intervalNum(detail[10])
-            customs.mylaj_sb_decile = init_quantile_mylaj_sb.intervalNum(detail[11])
-            customs.ckjhje_decile = init_quantile_ckjhje.intervalNum(detail[12])
-            customs.drrq = detail[13]
-            customs.nsrsbh = detail[14]
-            customs.dah = detail[15]
-            customs.jldwdm = detail[16]
-            customs.zglh = detail[17]
-            customs.sz = detail[18]
-            customs.jhpzh = detail[19]
-            customs.gfnsrsbh = detail[20]
-            customs.kprq = detail[21]
-            customs.jsje_decile = init_quantile_jsje.intervalNum(detail[22])
-            customs.zssl = detail[23]
-            customs.zsse_decile = init_quantile_zsse.intervalNum(detail[24])
-            customs.tsl = detail[25]
-            customs.ytse_decile = init_quantile_ytse.intervalNum(detail[24])
-            customs.hyd_sj = detail[27]
-            customs.hyd_dsj = detail[28]
-            customs.hyd_qxj = detail[29]
-            customs.ckrq_1 = detail[30]
-            customs.mylaj = detail[31]
-            customs.mylaj_decile = init_quantile_mylaj.intervalNum(detail[31])
-            customs.rmb = detail[32]
-            customs.sl1_decile = init_quantile_sl1.intervalNum(detail[33])
-            customs.sl2_decile = init_quantile_sl2.intervalNum(detail[34])
-            customs.sbsl_decile = init_quantile_sbsl.intervalNum(detail[35])
-            customs.mz_2_decile = init_quantile_mz_2.intervalNum(detail[36])
-            customs.jz_decile = init_quantile_jz.intervalNum(detail[37])
-            customs.zzmdgdqsz_dm = detail[38]
-            customs.ysfs_dm = detail[39]
-            customs.zyg_dm = detail[40]
-            customs.hgcjfs_dm = detail[41]
-            customs.jhfs_dm = detail[42]
-            customs.hzdwdq_dm = detail[43]
-            customs.hggqka_dm = detail[44]
-            customs.sbdw_dm = detail[45]
-            customs.sbdwmc = detail[46]
-            customs.ck_sb_diff = detail[47]
-            customs.fobdj_decile = init_quantile_fobdj.intervalNum(detail[48]) if detail[48] else ''
-            customs.zmy = float(zmy_frame[zmy_frame[:, 0] == detail[49]][:, 1]) if detail[49] else 0
-            customs.addList(customs.toList())
-        customs.save()
-        logger.info('》》》thread：%s ，目标明细数据保存成功！' % thread_name)
+                    WHERE HGQYDM = '""" + code + """' AND DATE_FORMAT(CKRQ_1, '%Y') = '""" + year + """'""")
+        logger.info('》》》thread：%s ，查询到该企业含有 %d 条报关单明细，开始明细分析' % (thread_name, len(details)))
+        details_group = listOfGroups(details, 2000)
+        threads = []
+        for details in details_group:
+            threads.append(Thread(target=saveTarget, args=(details,)))
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+
+
+def saveTarget(details):
+    thread_name = threading.current_thread().name
+    details_frame = numpy.array(details)
+    customs = CustomsDeclarationTarget()
+    for detail in details_frame:
+        customs.hgqydm = detail[0]
+        zmy_frame = total_trade_price[total_trade_price[:, 0] == detail[0]][:, 1:3]
+        customs.sbny = detail[1]
+        customs.pc = detail[2]
+        customs.tsjgdm = detail[3]
+        customs.glh = detail[4]
+        customs.ckfph = detail[5]
+        customs.bgdh = detail[6]
+        customs.spdm = detail[7]
+        customs.spmc = detail[8]
+        customs.jldw = detail[9]
+        customs.cksl_decile = init_quantile_cksl.intervalNum(detail[10])
+        customs.mylaj_sb_decile = init_quantile_mylaj_sb.intervalNum(detail[11])
+        customs.ckjhje_decile = init_quantile_ckjhje.intervalNum(detail[12])
+        customs.drrq = detail[13]
+        customs.nsrsbh = detail[14]
+        customs.dah = detail[15]
+        customs.jldwdm = detail[16]
+        customs.zglh = detail[17]
+        customs.sz = detail[18]
+        customs.jhpzh = detail[19]
+        customs.gfnsrsbh = detail[20]
+        customs.kprq = detail[21]
+        customs.jsje_decile = init_quantile_jsje.intervalNum(detail[22])
+        customs.zssl = detail[23]
+        customs.zsse_decile = init_quantile_zsse.intervalNum(detail[24])
+        customs.tsl = detail[25]
+        customs.ytse_decile = init_quantile_ytse.intervalNum(detail[24])
+        customs.hyd_sj = detail[27]
+        customs.hyd_dsj = detail[28]
+        customs.hyd_qxj = detail[29]
+        customs.ckrq_1 = detail[30]
+        customs.mylaj = detail[31]
+        customs.mylaj_decile = init_quantile_mylaj.intervalNum(detail[31])
+        customs.rmb = detail[32]
+        customs.sl1_decile = init_quantile_sl1.intervalNum(detail[33])
+        customs.sl2_decile = init_quantile_sl2.intervalNum(detail[34])
+        customs.sbsl_decile = init_quantile_sbsl.intervalNum(detail[35])
+        customs.mz_2_decile = init_quantile_mz_2.intervalNum(detail[36])
+        customs.jz_decile = init_quantile_jz.intervalNum(detail[37])
+        customs.zzmdgdqsz_dm = detail[38]
+        customs.ysfs_dm = detail[39]
+        customs.zyg_dm = detail[40]
+        customs.hgcjfs_dm = detail[41]
+        customs.jhfs_dm = detail[42]
+        customs.hzdwdq_dm = detail[43]
+        customs.hggqka_dm = detail[44]
+        customs.sbdw_dm = detail[45]
+        customs.sbdwmc = detail[46]
+        customs.ck_sb_diff = detail[47]
+        customs.fobdj_decile = init_quantile_fobdj.intervalNum(detail[48]) if detail[48] else ''
+        customs.zmy = float(zmy_frame[zmy_frame[:, 0] == detail[49]][:, 1]) if detail[49] else 0
+        customs.addList(customs.toList())
+    customs.save()
+    logger.info('》》》thread：%s ，目标明细数据保存成功！' % thread_name)
 
 
 global init_quantile_cksl, init_quantile_mylaj_sb, init_quantile_ckjhje, init_quantile_jsje, init_quantile_zsse, \
